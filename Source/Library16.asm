cfa2156f83411e4ef7f6badd36d922884f0e563d
Saurav Roy <xp.saurav@yahoo.in>
2021-01-20 02:30:55 +0530

    Changes to be committed: 	modified:   Source/Golden_Gate_Intercontinental.asm 	deleted:    Source/Include/Golden_Gate_Intercontinental/Interfaces/API_16.asm 	modified:   Source/Library16.asm
     Untracked files:
    	Source/Include/Golden_Gate_Intercontinental/Interfaces/

--- Source/Library16.asm
+++ Source/Library16.asm
@@ -1,5 +1,5 @@
 ;***********************************************************************
-%include "Include/Golden_Gate_Intercontinental/Interfaces/API_16.asm"
+%include "Include/Golden_Gate_Intercontinental/Interfaces/Operation_16_Bits.asm"
 %include "Include/BIOS/BIOS_10h.asm"
 %include "Include/BIOS/BIOS_15h.asm"
 %include "Include/BIOS/BIOS_CGA_16Colors.asm"
